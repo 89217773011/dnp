f4c80ab75edb744a6549c59b124f84c62daaab57
Kirill Maksimov <kirillm@realconnex.com>
2019-06-05 16:23:14 +0300

    Make home(without images)

---
+++ src/home/tileData.js
@@ -0,0 +1,28 @@
+import image from '../assets/pica.jpg';
+
+export const tileData = [
+  {
+    img: image,
+    title: 'Image1',
+    author: 'author',
+    cols: 2,
+  },
+  {
+    img: image,
+    title: 'Image2',
+    author: 'author',
+    cols: 3,
+  },
+  {
+    img: image,
+    title: 'Image3',
+    author: 'author',
+    cols: 3,
+  },
+  {
+    img: image,
+    title: 'Image4',
+    author: 'author',
+    cols: 2,
+  }
+];
